bdb722414daf9189ce3537990fab88e0213e80c5
François Chevalier <fchevalier12@gmail.com>
2015-10-15 22:44:26 +0200

    Test commit and push

--- server.py
+++ server.py
@@ -1,1 +1,11 @@
 __author__ = 'francoischevalier'
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, World")
+
+
+application = tornado.web.Application([])
