12022dc3fc09752e34bc2a82039b7b70af397956
François Chevalier <fchevalier12@gmail.com>
2015-10-17 14:25:16 +0200

    Websocket part added

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 __author__ = 'francoischevalier'
 import tornado.ioloop
 import tornado.web
+import tornado.websocket
+import ConfigParser
+
+
+uavVehicle = None
+config = None
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -8,4 +14,29 @@ class MainHandler(tornado.web.RequestHandler):
         self.write("Hello, World")
 
 
-application = tornado.web.Application([])
+class WSHandler(tornado.websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    def open(self, *args, **kwargs):
+        print("Connection Opened")
+        config = ConfigParser.ConfigParser()
+        config.read('uav_vehicle_config.cfg')
+        print(config.get("test", "test1"))
+
+    def on_close(self):
+        print("Connection closed")
+
+    def on_message(self, message):
+        print(message)
+
+
+application = tornado.web.Application([
+    (r'/', MainHandler),
+    (r'/ws', WSHandler),
+])
+
+
+if __name__ == '__main__':
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
